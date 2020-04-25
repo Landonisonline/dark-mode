e60de9a7c71b10b6695d40d5bff3737d5951a1fa
landonisonline <53879728+Landonisonline@users.noreply.github.com>
2020-04-25 13:35:41 -0500

    first commmit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function darkmode() {
+document.body.style.backgroundColor = "#3b3a3a";
+document.body.style.color = "white";
+document.body.style.transition= "background-color 1s, color 0.2s";
+}
+
+function lightmode() {
+document.body.style.backgroundColor = "white";
+document.body.style.color = "black";
+document.body.style.transition= "background-color 1s, color 0.2s";
+}
